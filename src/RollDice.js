ba2e0fa12220d1457a13c51f6232348aba063715
Derek <nightwing917@hotmail.com>
2022-05-26 19:33:43 -0800

    adding constructor to Roll Dice js

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class RollDice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {die1: 'one', die2: 'one'};
+    }
     render() {
         
     }
