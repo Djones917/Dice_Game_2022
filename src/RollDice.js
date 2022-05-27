2342e41231cee3b776bc049b37c0d4425fa351f6
Derek <nightwing917@hotmail.com>
2022-05-26 19:40:45 -0800

    added return with die1 and die1

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Die from './Die';
 
 class RollDice extends Component {
     constructor(props) {
@@ -6,6 +7,11 @@ class RollDice extends Component {
         this.state = {die1: 'one', die2: 'one'};
     }
     render() {
-        
+        return (
+            <div>
+              <Die face={this.state.die1}/>
+              <Die face={this.state.die2}/>
+            </div>
+        )
     }
 }
