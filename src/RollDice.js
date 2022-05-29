749cadbe9c18a4ba470202f0f4208297471aefe0
Derek <nightwing917@hotmail.com>
2022-05-28 19:39:25 -0800

    added static defaultProps

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Die from './Die';
 
 class RollDice extends Component {
+    static defaultProps = {
+       sides: ["one", "two", "three", "four", "five", "six"]
+    };
     constructor(props) {
         super(props);
         this.state = {die1: 'one', die2: 'one'};
