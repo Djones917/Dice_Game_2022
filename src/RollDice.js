eeb72233a7ecb4c7b9d3eb524e44eceb00b3ed4f
Derek <nightwing917@hotmail.com>
2022-05-30 20:05:26 -0800

    added newdie1

--- src/RollDice.js
+++ src/RollDice.js
@@ -11,6 +11,7 @@ class RollDice extends Component {
     }
     roll() {
         // Pick two new rolls
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
         // Set State with new rolls
     }
     render() {
